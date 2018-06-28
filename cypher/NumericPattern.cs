c40b2ba765489752a9846f4bc222d1014f426c9d
Adam Rushby (cptdagon) <adam.b.rushby@gmail.com>
2018-06-28 20:35:39 +0100

    moved pattern ananlyser to new file and seperate compariters into methods

---
+++ cypher/NumericPattern.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Linq;
+
+namespace PassCypher
+{
+    class NumericPattern
+    {
+        public static int NumberPattern(string password)
+        {
+            int[] iArray = new int[20];
+            VarInit(out int altres, out int sumres, out int sumpos, out int sumneg, out int i, out int j);
+
+            string numbers = new string(password.Where(c => char.IsDigit(c)).ToArray());
+
+            foreach (char c in numbers)
+            {
+                iArray[i] = (int)char.GetNumericValue(c);
+                i++;
+            }
+
+            SumOf(iArray, ref j, ref altres, ref sumres, ref sumpos, ref sumneg);
+
+            int N = i;
+            int posneg = Math.Abs(sumpos - sumneg);
+            int sda = DivBy0Catch(altres, sumres);
+
+            altres = Math.Abs(altres);
+            sda = Math.Abs(sda);
+            int avg = (sumres / N);
+
+            int score = 0;
+            if (altres == 0 || avg == 0)
+            {
+                score = 2;
+            }
+            if (posneg == altres)
+            {
+                score++;
+                score++;
+            }
+            if (posneg == avg)
+            {
+                score++;
+            }
+            if (altres == avg)
+            {
+                score++;
+                score++;
+            }
+            if ((sda * avg) == sumres)
+            {
+                score++;
+            }
+            if ((sumpos == 0) || (sumneg == 0))
+            {
+                if (avg == sda)
+                {
+                    score++;
+                }
+            }
+            if ((altres + posneg) == (sumpos))
+            {
+                score++;
+            }
+            if ((altres + posneg) == (sumneg))
+            {
+                score++;
+            }
+            if ((avg + sda) == Math.Ceiling((double)sumres / 2) || (avg + sda) == N)
+            {
+                score++;
+                score++;
+            }
+            if ((((sumres / posneg) + 1) == sumneg) || (((sumres / posneg) + 1) == sumpos))
+            {
+                if ((((sumres / posneg) - 1) == sumneg) || (((sumres / posneg) - 1) == sumpos))
+                {
+                    score++;
+                }
+            }
+
+            if ((N <= 2) && (score >= 3))
+            {
+                score = 2;
+            }
+            if (score >= 3)
+            {
+                return -1;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+        private static int DivBy0Catch(int altres, int sumres)
+        {
+            int sda = 0;
+
+            try
+            {
+                sda = (sumres / altres);
+            }
+            catch (DivideByZeroException)
+            {
+                sda = (sumres / 1);
+            }
+
+            return sda;
+        }
+
+        private static void VarInit(out int altres, out int sumres, out int sumpos, out int sumneg, out int i, out int j)
+        {
+            altres = 0;
+            sumres = 0;
+            sumpos = 0;
+            sumneg = 0;
+            i = 0;
+            j = 1;
+        }
+
+        private static void SumOf(int[] iArray, ref int j, ref int altres, ref int sumres, ref int sumpos, ref int sumneg)
+        {
+            foreach (int a in iArray)
+            {
+                if (j % 2 == 1)
+                {
+                    altres += a;
+                }
+                else
+                {
+                    altres -= a;
+                }
+                if (a % 2 == 1)
+                {
+                    sumneg += a;
+                }
+                else
+                {
+                    sumpos += a;
+                }
+                sumres += a;
+                j++;
+            }
+        }
+    }
+}
--- cypher/PasswordScore.cs
+++ cypher/PasswordScore.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace PassCypher
@@ -58,7 +57,7 @@ namespace PassCypher
             if (Regex.Match(password, @"\d+", RegexOptions.ECMAScript).Success)
             {
                 score++;
-                score += NumberPattern(password);
+                score += NumericPattern.NumberPattern(password);
             }
 
             if (Regex.Match(password, @"[a-z]", RegexOptions.ECMAScript).Success &&
@@ -73,128 +72,6 @@ namespace PassCypher
             }
 
             return (PassScore)score;
-        }
-
-        private static int NumberPattern(string password)
-        {
-            int[] iArray = new int[20];
-            int i = 0;
-            string numbers = new string(password.Where(c => char.IsDigit(c)).ToArray());
-
-            foreach (char c in numbers)
-            {
-                iArray[i] = (int)char.GetNumericValue(c);
-                i++;
-            }
-
-            int j = 1;
-            int altres = 0;
-            int sumres = 0;
-            int sumpos = 0;
-            int sumneg = 0;
-
-            foreach (int a in iArray)
-            {
-                if (j % 2 == 1)
-                {
-                    altres += a;
-                }
-                else
-                {
-                    altres -= a;
-                }
-                if (a % 2 == 1)
-                {
-                    sumneg += a;
-                }
-                else
-                {
-                    sumpos += a;
-                }
-                sumres += a;
-                j++;
-            }
-
-            int N = i;
-            int posneg = Math.Abs(sumpos - sumneg);
-            int sda = 0;
-
-            try
-            {
-                sda = (sumres / altres);
-            }
-            catch (DivideByZeroException)
-            {
-                sda = (sumres / 1);
-            }
-
-            altres = Math.Abs(altres);
-            sda = Math.Abs(sda);
-            int avg = (sumres / N);
-
-            int score = 0;
-            if (altres == 0 || avg == 0)
-            {
-                score = 2;
-            }
-            if (posneg == altres)
-            {
-                score++;
-                score++;
-            }
-            if (posneg == avg)
-            {
-                score++;
-            }
-            if (altres == avg)
-            {
-                score++;
-                score++;
-            }
-            if ((sda * avg) == sumres)
-            {
-                score++;
-            }
-            if ((sumpos == 0) || (sumneg == 0))
-            {
-                if (avg == sda)
-                {
-                    score++;
-                }
-            }
-            if ((altres + posneg) == (sumpos))
-            {
-                score++;
-            }
-            if ((altres + posneg) == (sumneg))
-            {
-                score++;
-            }
-            if ((avg + sda) == Math.Ceiling((double)sumres / 2) || (avg + sda) == N)
-            {
-                score++;
-                score++;
-            }
-            if ((((sumres / posneg) + 1) == sumneg) || (((sumres / posneg) + 1) == sumpos))
-            {
-                if ((((sumres / posneg) - 1) == sumneg) || (((sumres / posneg) - 1) == sumpos))
-                {
-                    score++;
-                }
-            }
-
-            if ((N <= 2) && (score >= 3))
-            {
-                score = 2;
-            }
-            if(score >= 3)
-            {
-                return -1;
-            }
-            else
-            {
-                return 1;
-            }
-        }
+        }              
     }   
 }
